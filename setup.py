29d06c1e43fa5fec09fdc5210358cf09051ba374
Ryan Bonham <ryan@transparent-tech.com>
2018-04-11 12:48:07 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='flask-app-core',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
     # The name of your scipt, and also the command you'll be using for calling it
-    scripts=['flask_app_core']
+    packages=['flask_app_core']
 )
